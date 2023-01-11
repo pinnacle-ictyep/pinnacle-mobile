e696f821cba1b542672d253f2aed903fc6f1a6bf
Ringsluv <ringssolomon5683@gmail.com>
2023-01-11 14:00:26 +0100

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+        title: 'Stayinn',
         theme: ThemeData(
+          useMaterial3: true,
           primarySwatch: Colors.blue,
         ),
         home: SplashScreen();
