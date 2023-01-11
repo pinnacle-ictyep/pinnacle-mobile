dcc6e64ee3acc23bf4186cf3228c8f774c45e51d
Ringsluv <ringssolomon5683@gmail.com>
2023-01-11 13:55:23 +0100

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
         title: 'STAYINN',
         theme: ThemeData(
+          useMaterial3: true,
           primarySwatch: Colors.blue,
         ),
         home: SplashScreen();
